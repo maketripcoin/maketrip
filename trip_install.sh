fdf64bfc7d5ae7adb1854efdb19f5e26e0ae70fa
maketripcoin <39806121+maketripcoin@users.noreply.github.com>
2018-09-03 21:06:01 +0100

    Update trip_install.sh

--- trip_install.sh
+++ trip_install.sh
@@ -10,8 +10,8 @@ COIN_PATH='/usr/local/bin/'
 COIN_TGZ="https://github.com/maketripcoin/trip/releases/download/2.0.1/trip-linux-x64.tar.gz"
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='Trip'
-COIN_PORT=51500
-RPC_PORT=52000
+COIN_PORT=41000
+RPC_PORT=40500
 
 NODEIP=$(curl -s4 api.ipify.org)
 
